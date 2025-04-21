954a20cb4792f3f0fbad84502266d6c3aec6d452
HubBrandzEx <114839193+hubbrandzex@users.noreply.github.com>
2025-04-21 13:38:01 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDmV1VihPju_r2F12a03z6CNzcuKOHmhng",
+    authDomain: "gtsniperbot-109a5.firebaseapp.com",
+    projectId: "gtsniperbot-109a5",
+    storageBucket: "gtsniperbot-109a5.firebasestorage.app",
+    messagingSenderId: "229720296197",
+    appId: "1:229720296197:web:cadaa92d928c6ea6d5456c"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  

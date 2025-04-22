5dfcf98d021aee8fe0e07dc6bb62d1ae2d313c65
HubBrandzEx <114839193+hubbrandzex@users.noreply.github.com>
2025-04-22 03:47:17 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,9 @@
-// firebase-config.js
-
-// Replace each placeholder with your real Firebase project settings
-export const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyDmV1VihPju_r2F12a03z6CNzcuKOHmhng",
     authDomain: "gtsniperbot-109a5.firebaseapp.com",
-    databaseURL: "https://gtsniperbot-109a5-default-rtdb.firebaseio.com",
     projectId: "gtsniperbot-109a5",
     storageBucket: "gtsniperbot-109a5.appspot.com",
     messagingSenderId: "229720296197",
     appId: "1:229720296197:web:cadaa92d928c6ea6d5456c"
-  };
-  
+};
+export default firebaseConfig;
